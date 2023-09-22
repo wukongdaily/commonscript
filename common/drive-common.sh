a91d548dcd83048d33d3f4a185e3b87fd64d5cd5
wukongdaily <143675923+wukongdaily@users.noreply.github.com>
2023-09-22 13:10:54 +0800

    Update drive-common.sh

--- common/drive-common.sh
+++ common/drive-common.sh
@@ -8,4 +8,4 @@ check_bash_installed() {
   fi
 }
 check_bash_installed
-wget -O /tmp/common.run  https://ghproxy.com/https://raw.githubusercontent.com/wukongdaily/commonscript/master/common/common.run && chmod +x /tmp/common.run  && /tmp/common.run
+wget -O /tmp/common.sh  https://ghproxy.com/https://raw.githubusercontent.com/wukongdaily/commonscript/master/common/common.sh && chmod +x /tmp/common.sh  && /tmp/common.sh
